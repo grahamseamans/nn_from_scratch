2b2251817801df19234ea0f99ad9365857ec09f1
Graham Seamans <seamansgraham@gmail.com>
2020-09-03 08:42:27 -0700

    removed bad testing bloat.

--- try1.py
+++ try1.py
@@ -25,6 +25,7 @@ test_images = idx2numpy.convert_from_file('./MNIST/train-images-idx3-ubyte')
 test_labels = idx2numpy.convert_from_file('./MNIST/train-labels-idx1-ubyte')
 
 flattened_test = []
+
 for row in test_images:
     flattened_test.append(row.flatten())
 
@@ -34,7 +35,7 @@ weights1 = np.random.normal(mu, sigma, (784, 10))
 biases1 = np.random.normal(mu, sigma, (10, ))
 
 out = layer_process(flattened_test[0], weights1, biases1)
-print(out)
-
 out = normalize(out)
 print(out)
+
+
